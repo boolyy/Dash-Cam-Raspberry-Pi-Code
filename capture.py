4f6cf161e618692f2b73a2df2b52bf10f969a015
NayaabC <61812897+NayaabC@users.noreply.github.com>
2021-02-19 15:25:15 -0500

    Camera
    Barebones camera script. records until the designated key is pressed. Webcam should be on before running the program.

---
+++ capture.py
@@ -0,0 +1,34 @@
+#Very barebones camera script.
+
+import cv2, time
+
+#creates video object, 0 for external camera
+video = cv2.VideoCapture(0)
+
+#time variable
+a = 0
+while True:
+    a = a + 1
+
+    check, frame = video.read() #creates a frame object
+
+    print(check)
+
+    # Prints matrix representation of the image
+    print(frame)
+
+    #Shows the frame
+    cv2.imshow("Capturing", frame)
+
+    key = cv2.waitKey(1)
+
+    #breaks streaming when q is pressed. Change key when necessary
+    if key == ord('q'):
+        break
+
+#prints streaming time
+print(a)
+
+#Shuts down camera
+video.release()
+cv2.destroyAllWindows()
